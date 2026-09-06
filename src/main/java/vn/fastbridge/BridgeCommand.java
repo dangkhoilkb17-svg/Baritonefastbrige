b9bbc78b64fffb34c34a49995a25f15166d24bd3
dangkhoilkb17-svg <dangkhoilkb17@gmail.com>
2026-09-06 23:21:26 +0700

    Replace bridge stop command with F8 emergency stop

--- src/main/java/vn/fastbridge/BridgeCommand.java
+++ src/main/java/vn/fastbridge/BridgeCommand.java
@@ -22,7 +22,6 @@ public final class BridgeCommand {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, access) -> dispatcher.register(
             literal("bridge")
                 .executes(ctx -> { controller.start(config.defaultLength, config.defaultWidth); return 1; })
-                .then(literal("stop").executes(ctx -> { controller.stop(); return 1; }))
                 .then(literal("auto")
                     .then(argument("width", IntegerArgumentType.integer(1, config.maxWidth))
                         .executes(ctx -> { controller.autoStart(IntegerArgumentType.getInteger(ctx, "width")); return 1; })))
@@ -37,7 +36,6 @@ public final class BridgeCommand {
         String[] a = input.isBlank() ? new String[0] : input.split("\\s+");
         try {
             if (a.length == 0) controller.start(config.defaultLength, config.defaultWidth);
-            else if (a.length == 1 && a[0].equalsIgnoreCase("stop")) controller.stop();
             else if (a.length == 1) controller.start(Integer.parseInt(a[0]), config.defaultWidth);
             else if (a.length == 2 && a[0].equalsIgnoreCase("auto")) controller.autoStart(Integer.parseInt(a[1]));
             else if (a.length == 2) controller.start(Integer.parseInt(a[0]), Integer.parseInt(a[1]));
